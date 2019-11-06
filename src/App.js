9357bc336bee2f0cd78dfffce51a43aca28a7e13
Jucemar Dimon <jucemar.dimon@gmail.com>
2019-11-06 18:09:15 -0300

    Ajuste no clusterer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import Map from "./components/Map";
 import Header from "./components/Header";
+import Map from './components/Map'
 import Dashboard from "./components/Dashboard";
 import Footer from "./components/Footer";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { Container, Main, MainLeft } from "./styles";
+import React from 'react'
 
-const App = () => <p>teste</p>;
-/*   <Container>
+const App = () => 
+  <Container>
     <Header />
     <Main>
       <MainLeft>
@@ -15,5 +16,5 @@ const App = () => <p>teste</p>;
       </MainLeft>
       <Map />
     </Main>
-  </Container> */
+  </Container> 
 export default App;
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,12 +1,14 @@
+import React from "react"
 import { randomPoint } from "@turf/random";
 import MapGL, { Marker } from "@urbica/react-map-gl";
 import Cluster from "@urbica/react-map-gl-cluster";
+import { FaMapMarkerAlt } from "react-icons/fa";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { Component, Text } from "react";
+import Locations from '../../controller/locations'
 
-const bbox = [-160, -70, 160, 70];
-const points = randomPoint(50, { bbox }).features;
-points.forEach((point, index) => (point.id = index));
+const sc=Locations.filter(item=>item.codigo_ibge.toString().startsWith('4202'))
+console.log(sc)
 
 const token =
   "pk.eyJ1IjoianVjZW1hciIsImEiOiJjazJnbTI4amIwMG5pM2NvaHM3ZHk3Y2x2In0.aeDv31Xsir3Gvwm0kcs7BA";
@@ -38,23 +40,22 @@ export default class Map extends Component {
     };
   }
   render() {
-    return <p>teste</p>;
-    /*   <MapGL
+    return (
+      <MapGL
         style={{ width: "100%", height: "400px" }}
         mapStyle="mapbox://styles/mapbox/light-v9"
         accessToken={token}
         onViewportChange={viewport => this.setState({ viewport })}
         {...this.state.viewport}
       >
-        {points.map(point => (
-          <Marker
-            key={point.id}
-            longitude={point.geometry.coordinates[0]}
-            latitude={point.geometry.coordinates[1]}
-          >
-            <div style={style} />
-          </Marker>
-        ))}
-      </MapGL> */
+        <Cluster
+          radius={40}
+          extent={512}
+          nodeSize={64}
+          component={ClusterMarker}
+        />
+
+
+      </MapGL>)
   }
 }
